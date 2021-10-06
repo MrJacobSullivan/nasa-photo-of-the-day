33cce7562bc6d1e9095d521229ab19fbcfb70825
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-06 14:20:01 -0500

    added data fetch effect in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Card from './components/Card/Card'
+import { BASE_URL, API_KEY } from './constants'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const config = { params: { api_key: API_KEY } }
+    axios
+      .get(BASE_URL, config)
+      .then((res) => {
+        const data = res.data
+        setData(() => data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div className='App'>
       <p>Hello world</p>
+      <Card data={data} />
     </div>
   )
 }
---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = process.env.REACT_APP_API_KEY
