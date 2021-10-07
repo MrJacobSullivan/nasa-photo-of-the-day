8408c8f69a5a6afd18a99df67ab986037894cb22
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-07 13:24:57 -0500

    added Styled Components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+
 import App from './App'
+import theme from './theme'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+)
